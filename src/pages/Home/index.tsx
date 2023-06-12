c5f0aad70bf4d4b229771afa2f3edfd01675e18d
jhonata.carvalho <jhonata.carvalho@tascomtecnologia.com.br>
2023-06-12 15:48:00 -0300

    bottomsheet bugado

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useMemo, useRef } from "react";
 import MapView, { Marker } from "react-native-maps";
-import { Button, StyleSheet, View } from "react-native";
+import { Button, StyleSheet, View, Text } from "react-native";
 import { stations, accidents, locations } from "../../localization";
 import { Container, MenuButton } from "./styled";
 import { Ionicons } from "@expo/vector-icons";
-
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 type Location = {
   nome: string;
   latitude: number;
@@ -13,7 +13,21 @@ type Location = {
 export default function Home({ navigation }) {
   const latitudeRecife = -8.05428;
   const longitudeRecife = -34.8813;
-
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+  //////////////////////////
+  const SheetRef = useRef<BottomSheet>(null);
+  const snapPoints = useMemo(() => ["25%", "50%", "90%"], []);
+  const handleSheetChange = useCallback((index: number) => {
+    console.log("aa");
+    handleSnapPress(index + 1);
+  }, []);
+  ////////////////////////////
+  const handleSnapPress = useCallback((index) => {
+    SheetRef.current?.snapToIndex(index);
+  }, []);
+  const handleClosePress = useCallback(() => {
+    SheetRef.current?.close();
+  }, []);
   const trocar = () => {
     setLocation(stations);
   };
@@ -40,10 +54,29 @@ export default function Home({ navigation }) {
           />
         ))}
       </MapView>
-      <Button onPress={trocar} title="Switch" />
+      <BottomSheet
+        onChange={handleSheetChange}
+        ref={SheetRef}
+        enablePanDownToClose
+        snapPoints={snapPoints}
+      >
+        <BottomSheetView>
+          <Text>Awesome 🎉</Text>
+        </BottomSheetView>
+      </BottomSheet>
+      {/* <Button onPress={trocar} title="Switch" /> */}
       <MenuButton onPress={() => navigation.navigate("Menu")}>
         <Ionicons name="menu" size={48} color="#FFF" />
       </MenuButton>
     </Container>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+    backgroundColor: "grey",
+  },
+  contentContainer: {
+    alignItems: "center",
+  },
+});
