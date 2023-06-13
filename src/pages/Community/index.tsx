09583d6a10bcdfd553568f4041daea49c8718406
jhonata.carvalho <jhonata.carvalho@tascomtecnologia.com.br>
2023-06-13 03:04:51 -0300

    ajustes aaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/pages/Community/index.tsx
+++ src/pages/Community/index.tsx
@@ -1,21 +1,20 @@
 import React, { useEffect, useState } from "react";
 import {
   SafeAreaView,
-  StatusBar,
   Text,
   View,
   Image,
-  Modal,
   TouchableOpacity,
-  ScrollView,
   FlatList,
+  ActivityIndicator,
+  RefreshControl,
 } from "react-native";
-// import { FAB } from 'react-native-paper';
-// import Icon from 'react-native-vector-icons/FontAwesome';
-import { useNavigation, useIsFocused } from "@react-navigation/native";
+
+import { useNavigation } from "@react-navigation/native";
 import styles from "./styles";
-import axios from "axios";
+
 import { AntDesign } from "@expo/vector-icons";
+import { api } from "../../services/api";
 type Post = {
   descricao: string;
   id: string;
@@ -28,27 +27,37 @@ type Post = {
 
 function Community() {
   const navigation = useNavigation();
-  const isFocused = useIsFocused();
+  const [loading, setLoading] = useState<boolean>(false);
   const [imageUrls, setImageUrls] = useState<Post[]>([]);
-  const [modalVisible, setModalVisible] = useState(false);
 
-  // const handleFABPress = () => {
-  //   navigation.navigate('Postagem');
-  // };
+  const [refreshing, setRefreshing] = useState<boolean>(false);
 
   async function fetchPostagens() {
+    setLoading(true);
     try {
-      const response = await axios.get(
-        "https://urbtech-app.herokuapp.com/comunidade/selecionaPostagensDaComunidade/1"
+      const response = await api.get(
+        "/comunidade/selecionaPostagensDaComunidade/1"
       );
       setImageUrls(response.data);
     } catch (error) {
       console.error("Ocorreu um erro ao buscar as postagens:", error);
+    } finally {
+      setLoading(false);
     }
   }
+  const onRefresh = async () => {
+    setRefreshing(true);
+    try {
+      await fetchPostagens();
+    } catch (error) {
+      console.error("Ocorreu um erro ao buscar as postagens:", error);
+    } finally {
+      setRefreshing(false);
+    }
+  };
+
   useEffect(() => {
     fetchPostagens();
-    console.log(imageUrls);
   }, []);
 
   return (
@@ -72,48 +81,57 @@ function Community() {
           </View>
         </View>
         <View style={{ flex: 1 }}>
-          <FlatList
-            data={imageUrls}
-            renderItem={(data) => (
-              <View style={{ justifyContent: "flex-end" }}>
-                <Image
-                  style={styles.image}
-                  source={{ uri: data.item.imgUrl }}
-                />
-                <View
-                  style={{
-                    alignSelf: "center",
-                    alignItems: "center",
-                    paddingLeft: 10,
-                    position: "absolute",
-                    width: "90%",
-                    height: 50,
-                    backgroundColor: "#FFF",
-                    opacity: 0.8,
-                    borderRadius: 8,
-                    bottom: 16,
-                    gap: 8,
-                    flexDirection: "row",
-                  }}
-                >
+          {loading ? (
+            <View style={{ justifyContent: "center", flex: 1 }}>
+              <ActivityIndicator size={44} color={"#323232"} />
+            </View>
+          ) : (
+            <FlatList
+              data={imageUrls}
+              renderItem={(data) => (
+                <View style={{ justifyContent: "flex-end" }}>
                   <Image
-                    style={{
-                      width: 30,
-                      height: 30,
-                      borderRadius: 30 / 2,
-                    }}
-                    source={{
-                      uri: data.item.imgUsuario,
-                    }}
+                    style={styles.image}
+                    source={{ uri: data.item.imgUrl }}
                   />
-                  <Text style={{ fontWeight: "bold" }}>
-                    {data.item.nomeUsuario}:
-                  </Text>
-                  <Text>{data.item.descricao}</Text>
+                  <View
+                    style={{
+                      alignSelf: "center",
+                      alignItems: "center",
+                      paddingLeft: 10,
+                      position: "absolute",
+                      width: "90%",
+                      height: 50,
+                      backgroundColor: "#FFF",
+                      opacity: 0.8,
+                      borderRadius: 8,
+                      bottom: 16,
+                      gap: 8,
+                      flexDirection: "row",
+                    }}
+                  >
+                    <Image
+                      style={{
+                        width: 30,
+                        height: 30,
+                        borderRadius: 30 / 2,
+                      }}
+                      source={{
+                        uri: data.item.imgUsuario,
+                      }}
+                    />
+                    <Text style={{ fontWeight: "bold" }}>
+                      {data.item.nomeUsuario}:
+                    </Text>
+                    <Text>{data.item.descricao}</Text>
+                  </View>
                 </View>
-              </View>
-            )}
-          />
+              )}
+              refreshControl={
+                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+              }
+            />
+          )}
         </View>
         <TouchableOpacity
           onPress={() => navigation.navigate("CreatePost")}
@@ -128,16 +146,6 @@ function Community() {
           <AntDesign name="pluscircle" size={62} color="#161B31" />
         </TouchableOpacity>
       </SafeAreaView>
-
-      {/* <FAB
-        onPress={handleFABPress}
-        style={styles.fab}
-        small={false}
-        icon={({ size }) => (
-          <Icon style={{ alignSelf: 'center' }} name="plus" size={size} color="#F3FCE7" />
-        )}
-        theme={{ colors: { accent: '#F3FCE7' } }}
-      /> */}
     </View>
   );
 }
